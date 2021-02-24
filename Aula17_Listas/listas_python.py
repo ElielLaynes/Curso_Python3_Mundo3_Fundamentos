b0c56e738e94a9af766d9a534f95fb65dd20ec61
Eliel Laynes <ElielLaynes@users.noreply.github.com>
2021-02-24 20:05:34 -0300

    Create listas_python.py

---
+++ Aula17_Listas/listas_python.py
@@ -0,0 +1,29 @@
+'''
+                    >>>>>>. LISTAS EM PYTHON <<<<<<<<
+
+-> Para declarar a lista, bastas abrir colchetes []. Exemplo var = []
+
+    # Listas ao contrário de tuplas, São mutáveis e podem ser alteradas.
+
+Exemplos:
+
+    -> O camando abaixo, lanche[3] = 'Picole', vai substituir o elemento no índice 3, no caso o Pudim, pelo Picolé na lista.
+
+    lanche = ['Hamburguer' , 'Suco', 'Pizza', 'Pudim' ]
+    lanche[3] = 'Picole'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
