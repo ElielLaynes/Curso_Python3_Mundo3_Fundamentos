342f1e061f3e94db1461ef81cdfcf8f403f9e4e1
Eliel Laynes <ElielLaynes@users.noreply.github.com>
2021-03-02 16:42:02 -0300

    Update desafio084.py

--- Aula17_Listas/desafio084.py
+++ Aula17_Listas/desafio084.py
@@ -3,9 +3,12 @@
 
 #   leia nome e peso de várias pessoas
 #   guarde tudo em uma lista
+#   Pergunte se quer continuar [s/n]
 
 # Depois mostre:
 
 #       A) QUANTAS pessoas foram cadastradas;
 #       B) Uma listagem com as pessoas mais pesadas;
 #       C) Uma listagem com as pessoas mais leves.
+
+
